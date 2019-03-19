2c3445c86eb4ae04385b823aeb60a9214de3a347
Felipe Montoya Alvarez <felipemontoyaalvarez@Woloxs-MacBook-Pro-6.local>
2019-03-19 14:02:06 -0500

    Creating the services needed and importing the angular http client

--- wolox-books/src/app/app.module.ts
+++ wolox-books/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './screens/register/register.component';
 import { RegisterFormComponent } from './screens/register/components/register-form/register-form.component';
-
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent,
@@ -15,7 +15,8 @@ import { RegisterFormComponent } from './screens/register/components/register-fo
   imports: [
     ReactiveFormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
