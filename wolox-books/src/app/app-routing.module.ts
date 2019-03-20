44b67e433599591248fb59f426b4eec96fcba05d
Felipe Montoya Alvarez <felipemontoyaalvarez@Woloxs-MacBook-Pro-6.local>
2019-03-20 13:26:19 -0500

    Adding the auth component and logout functionality

--- wolox-books/src/app/app-routing.module.ts
+++ wolox-books/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './screens/login/login.component';
 import { RegisterComponent } from './screens/register/register.component';
+import { AuthComponent } from './components/auth/auth.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full'},
   { path: 'login', component: LoginComponent, pathMatch: 'full' },
-  { path: 'sign-up', component: RegisterComponent, pathMatch: 'full' }
+  { path: 'sign-up', component: RegisterComponent, pathMatch: 'full' },
+  { path: 'auth', component: AuthComponent, pathMatch: 'full' }
 ];
 
 @NgModule({
---
+++ wolox-books/src/app/components/auth/auth.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { LocalStorageService } from 'src/app/services/local-storage.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-auth',
+  templateUrl: './auth.component.html',
+  styleUrls: ['./auth.component.scss']
+})
+export class AuthComponent{
+
+  constructor(private storage: LocalStorageService, private router: Router) { }
+
+  logout() {
+    this.storage.clearStorage();
+    this.router.navigateByUrl('');
+  }
+
+}
