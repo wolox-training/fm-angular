939b55ba3dee57f687a10f9fc9a034443400b2e2
Felipe Montoya Alvarez <felipemontoyaalvarez@Woloxs-MacBook-Pro-6.local>
2019-03-20 09:17:03 -0500

    Adding the api url as an environment variable

--- wolox-books/src/app/services/user.service.ts
+++ wolox-books/src/app/services/user.service.ts
@@ -1,16 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  readonly BASE_URL = 'https://wbooks-api-stage.herokuapp.com/api/v1';
-
   constructor(private http: HttpClient) { }
 
   createUser(user) {
-    return this.http.post(this.BASE_URL + '/users', user);
+    return this.http.post(environment.apiUrl + '/users', user);
   }
 }
