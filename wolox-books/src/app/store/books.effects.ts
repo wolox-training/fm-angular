31d04785c108338b09c2c7f926e6adf06c92e4da
Felipe Montoya Alvarez <felipemontoyaalvarez@Woloxs-MacBook-Pro-6.local>
2019-03-26 14:19:32 -0500

    Removing an unused import

--- wolox-books/src/app/store/books.effects.ts
+++ wolox-books/src/app/store/books.effects.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { Actions, Effect, ofType } from '@ngrx/effects';
-import { EMPTY } from 'rxjs';
 import { map, mergeMap } from 'rxjs/operators';
 import { BooksService } from '../services/books.service';
 import * as BookActions from './books.actions';
@@ -12,9 +11,8 @@ export class BooksEffects {
     .pipe(
       ofType(BookActions.GET_BOOKS),
       mergeMap(() => this.booksService.getBooks()
-        .pipe(
-          map(books => new BookActions.SetBooks(books))
-        ))
+        .pipe(map(books => new BookActions.SetBooks(books)))
+      )
     );
 
   constructor(
